1146e9165d602ebdbd635ed1119fd74cc2f2ee4e
Arturo Valverde <art.valverde.563@gmail.com>
2025-06-03 12:10:30 -0600

    Proyecto finalizado

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -135,9 +135,9 @@ class ActiveRecord
     // Obtener Registros con cierta cantidad
     public static function get($limite)
     {
-        $query = "SELECT * FROM " . static::$tabla . " LIMIT ${limite} ORDER BY id DESC";
+        $query = "SELECT * FROM " . static::$tabla . " ORDER BY id DESC LIMIT $limite";
         $resultado = self::consultarSQL($query);
-        return array_shift($resultado);
+        return $resultado;
     }
 
     //Paginar los Regustros
@@ -165,14 +165,26 @@ class ActiveRecord
         return $resultado;
     }
 
+    // Retornar los registros por orden y con un límite
+    public static function ordenarLimite($columna, $orden, $limite)
+    {
+        $query = "SELECT * FROM " . static::$tabla . " ORDER BY $columna $orden LIMIT $limite ";
+
+        $resultado = self::consultarSQL($query);
+        return $resultado;
+    }
+
     //Busqueda Where con Multiples opciones
     public static function whereArray($array = [])
     {
         $query = "SELECT * FROM " . static::$tabla . " WHERE ";
 
         foreach ($array as $key => $value) {
-            $query .= "$key = '$value'";
-            $query .= $key !== array_key_last($array) ? " AND " : '';
+            if ($key == array_key_last($array)) {
+                $query .= "$key = '$value' ";
+            } else {
+                $query .= "$key = '$value' AND ";
+            }
         }
 
         $resultado = self::consultarSQL($query);
@@ -186,6 +198,25 @@ class ActiveRecord
         if ($columna) {
             $query .= " WHERE $columna = $valor";
         }
+        $resultado = self::$db->query($query);
+        $total = $resultado->fetch_array();
+
+        return array_shift($total);
+    }
+
+    //Traer un total de registros con un array where
+    public static function totalArray($array = [])
+    {
+        $query = "SELECT COUNT(*) FROM " . static::$tabla . " WHERE ";
+
+        foreach ($array as $key => $value) {
+            if ($key == array_key_last($array)) {
+                $query .= "$key = '$value' ";
+            } else {
+                $query .= "$key = '$value' AND ";
+            }
+        }
+
         $resultado = self::$db->query($query);
         $total = $resultado->fetch_array();
 
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,1 +1,41 @@
-<h1><?= $titulo ?></h1>
+<h2 class="dashboard__heading"><?= $titulo ?></h2>
+
+<main class="bloques">
+    <div class="bloques__grid">
+        <div class="bloque">
+            <h3 class="bloque__heading">Últimos Registros</h3>
+
+            <?php foreach ($registros as $registro) : ?>
+
+                <div class="bloque__contenido">
+                    <p class="bloque__texto">
+                        <?= $registro->usuario->nombre . " " . $registro->usuario->apellido ?>
+                    </p>
+                </div>
+
+            <?php endforeach ?>
+        </div>
+        <div class="bloque">
+            <h3 class="bloque__heading">Ingresos</h3>
+            <p class="bloque__texto--cantidad">$ <?= $ingresos ?></p>
+        </div>
+        <div class="bloque">
+            <h3 class="bloque__heading">Eventos con Menos Lugares Disponibles</h3>
+            <?php foreach ($menos_disponibles as $evento) : ?>
+                <p class="bloque__texto">
+                    <?= "$evento->nombre - $evento->disponibles"  ?>
+                </p>
+
+            <?php endforeach ?>
+        </div>
+        <div class="bloque">
+            <h3 class="bloque__heading">Eventos con Más Lugares Disponibles</h3>
+            <?php foreach ($mas_disponibles as $evento) : ?>
+                <p class="bloque__texto">
+                    <?= "$evento->nombre - $evento->disponibles"  ?>
+                </p>
+
+            <?php endforeach ?>
+        </div>
+    </div>
+</main>
